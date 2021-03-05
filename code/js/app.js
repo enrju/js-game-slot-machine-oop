31d8f29d5e95ae40a659c4ba63f350dd71b57062
enrju <enrju@enrju.pl>
2021-03-05 18:15:31 +0100

    feat(App): add object bid and tmp try input

--- code/js/app.js
+++ code/js/app.js
@@ -1,16 +1,22 @@
 class App{
     constructor(){
         this.page = new Page();
+        this.bid = new Bid();
 
         this.page.inpBid.addEventListener("input", this.inputGetValue.bind(this));
         this.page.btnPlay.addEventListener("click", this.clickBtnPlay.bind(this));
     }
 
     inputGetValue(){
+        // let val = this.page.inpBid.value;
+        // console.log(val);
 
+        // if(val === "") console.log('pusty');
+        // if(val == 1) console.log('1');
+        // if(val == 0) console.log('0');
     }
 
     clickBtnPlay(){
-        
+
     }
 }
