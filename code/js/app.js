782d4680b08aa9b966ab4dd9c35692433431d3f9
enrju <enrju@enrju.pl>
2021-03-05 17:41:48 +0100

    feat(App): add event listener "input" (empty)

--- code/js/app.js
+++ code/js/app.js
@@ -1,5 +1,13 @@
 class App{
     constructor(){
         this.page = new Page();
+
+        this.page.inpBid.addEventListener("input", this.inputGetValue.bind(this));
+    }
+
+    inputGetValue(){
+
+    }
+
     }
 }
