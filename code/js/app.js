e88230aafdebbc55b88f04c28917966d7f7c0d10
enrju <enrju@enrju.pl>
2021-03-05 17:42:58 +0100

    feat(App): add event listener "click" (empty)

--- code/js/app.js
+++ code/js/app.js
@@ -3,11 +3,14 @@ class App{
         this.page = new Page();
 
         this.page.inpBid.addEventListener("input", this.inputGetValue.bind(this));
+        this.page.btnPlay.addEventListener("click", this.clickBtnPlay.bind(this));
     }
 
     inputGetValue(){
 
     }
 
+    clickBtnPlay(){
+        
     }
 }
