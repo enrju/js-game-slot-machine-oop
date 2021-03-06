879ab3af33295500023a51b3b336ea87d506df12
enrju <enrju@enrju.pl>
2021-03-06 14:29:45 +0100

    feat(Stats): update method getStatSummary()

--- code/js/Stats.js
+++ code/js/Stats.js
@@ -8,29 +8,29 @@ class Stats{
     }
 
     getStatSummary(){
-        let NGames = 0;
-        let NWins = 0;
-        let NLosses = 0;
+        let nGames = 0;
+        let nWins = 0;
+        let nLosses = 0;
         let tmp;
 
-        NGames = this.results.length;
+        nGames = this.results.length;
 
         tmp = this.results.filter((item)=>{
             if(item === "win") return true;
             else return false;
         });
-        NWins = tmp.length;
+        nWins = tmp.length;
 
         tmp = this.results.filter((item)=>{
             if(item === "loss") return true;
             else return false;
         });
-        NLosses = tmp.length;
+        nLosses = tmp.length;
 
         return {
-            NGames,
-            NWins,
-            NLosses
+            nGames,
+            nWins,
+            nLosses
         }
     }
 }
