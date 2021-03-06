80ee69ff639eb2a81edd4ccd23dc2cd7ab47838b
enrju <enrju@enrju.pl>
2021-03-06 15:27:20 +0100

    feat(Warnings): update constructor()

--- code/js/Warnings.js
+++ code/js/Warnings.js
@@ -1,12 +1,15 @@
 class Warnings{
     constructor(){
-        this.texts = {
-            overbid: "you haven't enough points",
-            emptybid: "you didn't put bid, put it please"
-        };
+        this.index = {
+            overbid: 0,
+            emptybid: 1
+        }
+        this.texts = [
+            "you haven't enough points",
+            "you didn't put bid, put it please"
+        ];
     }
 
     getWarning(which) {
-        return this.texts.which;
     }
 }
