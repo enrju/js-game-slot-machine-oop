5488bb92092c4af4684d59f09bdad1a6a3da819c
enrju <enrju@enrju.pl>
2021-03-06 12:52:01 +0100

    feat(Stats): add method getStatSummary()

--- code/js/Stats.js
+++ code/js/Stats.js
@@ -7,4 +7,30 @@ class Stats{
         this.results.push(result);
     }
 
+    getStatSummary(){
+        let NGames = 0;
+        let NWins = 0;
+        let NLosses = 0;
+        let tmp;
+
+        NGames = this.results.length;
+
+        tmp = this.results.filter((item)=>{
+            if(item === "win") return true;
+            else return false;
+        });
+        NWins = tmp.length;
+
+        tmp = this.results.filter((item)=>{
+            if(item === "loss") return true;
+            else return false;
+        });
+        NLosses = tmp.length;
+
+        return {
+            NGames,
+            NWins,
+            NLosses
+        }
+    }
 }
