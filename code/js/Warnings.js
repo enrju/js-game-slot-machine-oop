836443a238f0aaaa7c3c6bcb8ea30bdb12524e48
enrju <enrju@enrju.pl>
2021-03-07 19:59:34 +0100

    feat(Warnings): add comments

--- code/js/Warnings.js
+++ code/js/Warnings.js
@@ -1,16 +1,23 @@
+// What is does: store and manage warnings (texts)
 class Warnings{
     constructor(){
+        //index of warning
         this.index = {
             overbid: 0,
             emptybid: 1
         }
-        this.texts = [
+
+        //warnings
+        this.warnings = [
             "you haven't enough points",
             "you didn't put bid, put it please"
         ];
     }
 
-    getWarning(which) {
-        return this.texts[which];
+    // What is does: get one warning from table
+    //        input: index - index warning in table
+    //       output: warning
+    getWarning(index) {
+        return this.warnings[index];
     }
 }
