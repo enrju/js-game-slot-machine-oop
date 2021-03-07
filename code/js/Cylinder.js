2dc1471009fdecc3edd0e38b2d8958cb5918b3f6
enrju <enrju@enrju.pl>
2021-03-07 19:56:23 +0100

    feat(Cylinder): add comments

--- code/js/Cylinder.js
+++ code/js/Cylinder.js
@@ -1,3 +1,4 @@
+// What is does: store and manage single cylinder
 class Cylinder{
     constructor(heightTale, nTales){
         this.heightTale = heightTale;
@@ -6,15 +7,25 @@ class Cylinder{
         this.nowPosition = this.startPosition;
     }
 
+    // What is does: calc position of cylinder
+    //        input: none
+    //       output: position in [px]
     calcPosition(){
         let tale = this.numberRandomFrom(0, this.nTales - 1);
         return tale * this.heightTale;
     }
 
+    // What is does: animation of cylinder
+    //        input: none
+    //       output: none
     animate(){
-
+        //unused in this version
     }
 
+    // What is does: get random number from range (includes max)
+    //        input: min - min number of range
+    //               max - max number of range
+    //       output: random number from min-max
     numberRandomFrom(min, max) {
         const number = Math.random() * (max - min + 1);
         return Math.floor(number);
