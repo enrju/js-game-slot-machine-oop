a90cac08b288bd95c19dbb63d3b2196d780d47c6
enrju <enrju@enrju.pl>
2021-03-07 12:50:47 +0100

    feat(Result): update methods check() and calc()

--- code/js/Result.js
+++ code/js/Result.js
@@ -5,16 +5,24 @@ class Result{
         this.winFactor = factor;
     }
 
-    check(cylinders){
+    check(cylinders, bid){
+        //for 3 cylinders
+        if(cylinders[0].nowPosition === cylinders[1].nowPosition && cylinders[0].nowPosition === cylinders[2].nowPosition){
+            this.state = "win";
+        }
+        else{
+            this.state = "loss";
+        }
 
+        this.calc(bid);
     }
 
     calc(bid){
         if(this.state === "win"){
-            this.value = bid * this.winFactor;
+            this.value = bid.value * this.winFactor;
         }
         else{
-            this.value = bid;
+            this.value = bid.value;
         }
     }
 }
